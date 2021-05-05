7207a216d9b655608067a0736a8e1195a5c94756
Bősze Máté <bosze.mate1997@gmail.com>
2021-05-05 16:34:39 +0200

    Végre Rajzol

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
+import json
 
-def drawing(ImageName, Width, Height, CenterX, CenterY):
+
+def jsopen(filename) -> dict:
+    file = open(filename,'r')
+    file_contents = json.load(file)#dictionary-t készít belőle miután betölti a fájl adatait
+    file.close()
+    return file_contents
+
+
+panel1 = jsopen('Panels_1.json')
+
+p10i = panel1['Panels_1'][0]['Images']
+p11i = panel1['Panels_1'][1]['Images']
+p12 = panel1['Panels_1'][2]
+
+def drawing(ImageName):
     fig, ax = plt.subplots()
     im = Image.open(ImageName)
-    posx = (CenterY*im.height) - (CenterY*im.height)/2
-    posy = (CenterX*im.width)- (CenterX*im.width/2)
+    j = 0
+    found = False
+    while(j < len(p10i) and not found):
+        if p10i[j]['ImageName'] == ImageName:
+            for i in range(len (p10i[j]['BoundingBoxes'])):
+                CenterY = p10i[j]['BoundingBoxes'][i]['CenterY']
+                print(p10i[j]['BoundingBoxes'][i]['CenterY'])
+                CenterX = p10i[j]['BoundingBoxes'][i]['CenterX']
+                print(p10i[j]['BoundingBoxes'][i]['CenterX'])
+                Width = p10i[j]['BoundingBoxes'][i]['Width']
+                print(p10i[j]['BoundingBoxes'][i]['Width'])
+                Height = p10i[j]['BoundingBoxes'][i]['Height']
+                print(p10i[j]['BoundingBoxes'][i]['Height'])
+                posx = (CenterX*im.width) - ((Width*im.width)/2)
+                posy = (CenterY*im.height) - ((Height*im.height)/2)
+                rect = patches.Rectangle((posx, posy), Width*im.width, Height*im.height, linewidth=1, edgecolor='Green', facecolor='none')
+                ax.add_patch(rect)
+            found = True
+        j = j+1
     ax.imshow(im)
-    rect = patches.Rectangle((posx, posy), Height*im.height, Width*im.width, linewidth=1, edgecolor='Green', facecolor='none')
-    ax.add_patch(rect)
     plt.show()
 
 def main():
-    ImageName = '1_6.jpg'
-    Width = 0.0310077518
-    Height = 0.055552192
-    CenterX = 0.25
-    CenterY = 0.38377896
-    drawing(ImageName, Width, Height, CenterX, CenterY)
+    ImageName = ''
+    drawing(ImageName)
 
 if __name__ == "__main__":
     main()
