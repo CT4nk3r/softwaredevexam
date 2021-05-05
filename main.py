5d5e20cfd5779b0692f31084cca549dfd358a0c1
Bősze Máté <bosze.mate1997@gmail.com>
2021-05-05 16:58:09 +0200

    Coloring Done

--- main.py
+++ main.py
@@ -6,16 +6,37 @@ import json
 
 def jsopen(filename) -> dict:
     file = open(filename,'r')
-    file_contents = json.load(file)#dictionary-t készít belőle miután betölti a fájl adatait
+    file_contents = json.load(file)
     file.close()
     return file_contents
-
 
 panel1 = jsopen('Panels_1.json')
 
 p10i = panel1['Panels_1'][0]['Images']
 p11i = panel1['Panels_1'][1]['Images']
 p12 = panel1['Panels_1'][2]
+
+def color_switch(color_code):
+    if color_code == 0:
+        return 'Yellow'        
+    elif color_code == 2:
+        return 'Green'
+    elif color_code == 3:
+        return 'Blue'
+    elif color_code == 4:
+        return 'Lime'
+    elif color_code == 5:
+        return 'Lime'
+    elif color_code == 6:
+        return 'Lime'
+    elif color_code == 7:
+        return 'Cyan'
+    elif color_code == 8:
+        return 'Orange'
+    elif color_code == 9:
+        return 'Red'
+    else:
+        return 'Lime'
 
 def drawing(ImageName):
     fig, ax = plt.subplots()
@@ -35,7 +56,9 @@ def drawing(ImageName):
                 print(p10i[j]['BoundingBoxes'][i]['Height'])
                 posx = (CenterX*im.width) - ((Width*im.width)/2)
                 posy = (CenterY*im.height) - ((Height*im.height)/2)
-                rect = patches.Rectangle((posx, posy), Width*im.width, Height*im.height, linewidth=1, edgecolor='Green', facecolor='none')
+                color_code = p10i[j]['BoundingBoxes'][i]['ObjectIndex']
+                color = color_switch(color_code)
+                rect = patches.Rectangle((posx, posy), Width*im.width, Height*im.height, linewidth=1, edgecolor=color, facecolor='none')
                 ax.add_patch(rect)
             found = True
         j = j+1
@@ -43,7 +66,7 @@ def drawing(ImageName):
     plt.show()
 
 def main():
-    ImageName = ''
+    ImageName = '1_6.jpg'
     drawing(ImageName)
 
 if __name__ == "__main__":
