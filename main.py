c88fc581139c79a6d55fb9f908a7d0ce2fb8f29a
Bősze Máté <bosze.mate1997@gmail.com>
2021-05-05 14:56:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 
-im = Image.open('1_6.jpg')
+def drawing(ImageName, Width, Height, CenterX, CenterY):
+    fig, ax = plt.subplots()
+    im = Image.open(ImageName)
+    posx = (CenterY*im.height) - (CenterY*im.height)/2
+    posy = (CenterX*im.width)- (CenterX*im.width/2)
+    ax.imshow(im)
+    rect = patches.Rectangle((posx, posy), Height*im.height, Width*im.width, linewidth=1, edgecolor='Green', facecolor='none')
+    ax.add_patch(rect)
+    plt.show()
 
-def drawing():
-    pass
+def main():
+    ImageName = '1_6.jpg'
+    Width = 0.0310077518
+    Height = 0.055552192
+    CenterX = 0.25
+    CenterY = 0.38377896
+    drawing(ImageName, Width, Height, CenterX, CenterY)
 
-fig, ax = plt.subplots()
-Width = 0.0310077518
-Height = 0.055552192
-CenterX = 0.25
-CenterY = 0.38377896
-posx = (CenterY*im.Height) - (CenterY*im.Height)/2
-posy = (CenterX*im.Width)- (CenterX*im.Width/2)
-ax.imshow(im)
-
-rect = patches.Rectangle((posx, posy), height*im.height, width*im.width, linewidth=1, edgecolor='r', facecolor='none')
-
-ax.add_patch(rect)
-
-plt.show()
+if __name__ == "__main__":
+    main()
